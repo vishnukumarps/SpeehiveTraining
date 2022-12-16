8a317fa0142d1e1f964fe89074e2c491075a7b67
vishnukumar.ps@outlook.com <vishnukumar.ps@outlook.com>
2022-12-16 09:31:27 +0530

    functions

---
+++ Student-Mangement/Helper/Calculations.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Mangement.Helper
+{
+    public class Calculations
+    {
+        // 1 A Function Without Parameter But No Return Type
+
+        public void SayHello() // function body
+        {
+            Console.WriteLine("Hello ALL");
+
+        }
+       
+        // 2 A Function Without Parameter with  Return Type
+
+        public double getMeTemp()
+        {
+
+            return 101.20;
+        }
+       
+
+        // 3 A Function With Parameter no Return Type
+
+        public void  Sum( int a,int b)
+        {
+
+        }
+        // 4 A Function With Parameter with  Return Type
+
+        public int  multiplication(int a,int b)
+        {
+
+
+
+
+            return a * b;
+        }
+
+       
+        
+
+
+
+
+
+
+    }
+}
---
+++ Student-Mangement/Program.cs
@@ -0,0 +1,31 @@
+﻿using Student_Mangement.Helper;
+using System;
+
+
+namespace Student_Mangement
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calculations obj = new Calculations(); // object creation
+            obj.SayHello(); // function call
+            obj.multiplication(10, 200);
+
+            double number = obj.getMeTemp();
+            Console.WriteLine(number);
+            int x, y;
+
+            Console.WriteLine("Enter two num");
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+
+
+            obj.multiplication(x,y);
+
+
+            Console.ReadKey();
+        }
+    }
+}

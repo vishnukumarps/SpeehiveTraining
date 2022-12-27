e0d8c6fdfff009a802568604cb00741c60f81f7e
vishnukumar.ps@outlook.com <vishnukumar.ps@outlook.com>
2022-12-27 09:42:16 +0530

    Mvc example 2

---
+++ SampleApp1/Controllers/StudentController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SampleApp1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+        public IActionResult Sum(int a, int b)
+        {
+
+            int ans = a + b;
+
+
+            ViewBag.Ans1 = ans;
+
+            return View();
+        }
+
+
+        public IActionResult AddDetails( string studnetName, int rollNo)
+        {
+       
+
+
+            ViewBag.StudentName1 = studnetName;
+            ViewBag.RollNo2 = rollNo;
+            return View();
+        }
+
+    }
+}
